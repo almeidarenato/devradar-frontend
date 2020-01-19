26a5127d893aa5831298d9ef09dbf650add2e2bd
almeidarenato <renatoam@ymail.com>
2020-01-19 03:19:57 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
+
+//Conceitos do React
+// componente: bloco isolado de HTML , CSS e JS que nao interfere no restante da aplicaçao
+// estado: informações mantidas pelo componente (imutabilidade). Tota vez que há alteração do estado o componente é executado novamente.
+// propriedade: informaçoes que um componente pai passa para um componente filho
 
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+    setDevs([...devs, response.data]);
+  }
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <DevForm onSubmit={handleAddDev} />
+      </aside>
+      <main>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
---
+++ src/components/DevForm/index.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+function DevForm({ onSubmit }) {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [github_username, setGithubusername] = useState("");
+  const [techs, setTechs] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    onSubmit({
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+    setGithubusername("");
+    setTechs("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="input-block">
+        <label htmlFor="github_username">Usuário do GitHub</label>
+        <input
+          type="text"
+          name="github_username"
+          id="github_username"
+          value={github_username}
+          onChange={e => setGithubusername(e.target.value)}
+          required
+        />
+      </div>
+      <div className="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input
+          type="text"
+          name="techs"
+          id="techs"
+          value={techs}
+          onChange={e => setTechs(e.target.value)}
+          required
+        />
+      </div>
+      <div className="input-group">
+        <div className="input-block">
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            type="number"
+            name="latitude"
+            id="latitude"
+            value={latitude}
+            onChange={e => setLatitude(e.target.value)}
+            required
+          />
+        </div>
+        <div className="input-block">
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            type="number"
+            name="longitude"
+            id="longitude"
+            value={longitude}
+            onChange={e => setLongitude(e.target.value)}
+            required
+          />
+        </div>
+      </div>
+      <button type="submit">Salvar</button>
+    </form>
+  );
+}
+
+export default DevForm;
